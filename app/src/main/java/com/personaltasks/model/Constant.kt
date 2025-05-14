c49395953914ec3ecb08df848ce2a6dd6d4c4950
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-05-14 20:31:33 -0300

    feat: add Constant object with task-related constants

---
+++ app/src/main/java/com/personaltasks/model/Constant.kt
@@ -0,0 +1,7 @@
+package com.personaltasks.model
+
+object Constant {
+    const val EXTRA_TASK = "EXTRA_TASK"
+    const val EXTRA_VIEW_TASK = "EXTRA_VIEW_TASK"
+    const val INVALID_TASK_ID = -1
+}
