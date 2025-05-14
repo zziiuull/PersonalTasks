86f740e6ab0b245f106fe75073cf3e103bc0d385
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-05-14 20:32:09 -0300

    feat: implement TaskRvAdapter for managing task display and interactions

---
+++ app/src/main/java/com/personaltasks/adapter/TaskRvAdapter.kt
@@ -0,0 +1,65 @@
+package com.personaltasks.adapter
+
+import com.personaltasks.R
+import com.personaltasks.databinding.TileTaskBinding
+import com.personaltasks.model.Task
+import com.personaltasks.ui.OnTaskClickListener
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskRvAdapter(
+    private val taskList: MutableList<Task>,
+    private val onTaskClickListener: OnTaskClickListener
+): RecyclerView.Adapter<TaskRvAdapter.TaskViewHolder>() {
+    inner class TaskViewHolder(ttb: TileTaskBinding): RecyclerView.ViewHolder(ttb.root){
+        val titleTv: TextView = ttb.titleTv
+        val descriptionTv: TextView = ttb.descriptionTv
+//        val dueDate: TextView = ttb.dueDate
+
+        init {
+            ttb.root.setOnCreateContextMenuListener{ menu, v, menuInfo ->
+                (onTaskClickListener as AppCompatActivity).menuInflater.inflate(R.menu.context_menu, menu)
+                menu.findItem(R.id.edit_task_mi).setOnMenuItemClickListener {
+                    onTaskClickListener.onEditTaskMenuItemClick(adapterPosition)
+                    true
+                }
+                menu.findItem(R.id.remove_task_mi).setOnMenuItemClickListener {
+                    onTaskClickListener.onRemoveTaskMenuItemClick(adapterPosition)
+                    true
+                }
+            }
+            ttb.root.setOnClickListener { onTaskClickListener.onTaskClick(adapterPosition) }
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TaskViewHolder = TaskViewHolder(
+        TileTaskBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+    )
+    override fun onBindViewHolder(
+        holder: TaskViewHolder,
+        position: Int
+    ) {
+        taskList[position].let { task ->
+            with(holder) {
+                titleTv.text = task.title
+                descriptionTv.text = task.description
+//                .text = task.duedate
+                holder.itemView.setOnClickListener {
+                    onTaskClickListener.onTaskClick(position)
+                }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = taskList.size
+}
