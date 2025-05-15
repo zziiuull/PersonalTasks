5df538b4b8cfc5b9f9d92966d5ad2981c92bc9ec
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-05-15 17:01:31 -0300

    feat: implement task management functionality in MainActivity

--- app/src/main/java/com/personaltasks/ui/MainActivity.kt
+++ app/src/main/java/com/personaltasks/ui/MainActivity.kt
@@ -1,21 +1,132 @@
 package com.personaltasks.ui
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.personaltasks.R
+import com.personaltasks.adapter.TaskRvAdapter
+import com.personaltasks.controller.MainController
+import com.personaltasks.databinding.ActivityMainBinding
+import com.personaltasks.model.Constant.EXTRA_TASK
+import com.personaltasks.model.Constant.EXTRA_VIEW_TASK
+import com.personaltasks.model.Task
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnTaskClickListener {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val taskList: MutableList<Task> = mutableListOf()
+
+    private val taskAdapter: TaskRvAdapter by lazy {
+        TaskRvAdapter(taskList, this)
+    }
+
+    private lateinit var carl: ActivityResultLauncher<Intent>
+
+    private val mainController: MainController by lazy {
+        MainController(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(amb.root)
+
+        setSupportActionBar(amb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = "Task list"
+
+        carl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val contact = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    result.data?.getParcelableExtra(EXTRA_TASK, Task::class.java)
+                }
+                else {
+                    result.data?.getParcelableExtra<Task>(EXTRA_TASK)
+                }
+                contact?.let{ receivedTask ->
+                    val position = taskList.indexOfFirst { it.id == receivedTask.id }
+                    if (position == -1) {
+                        taskList.add(receivedTask)
+                        taskAdapter.notifyItemInserted(taskList.lastIndex)
+                        mainController.insertTask(receivedTask)
+                    }
+                    else {
+                        taskList[position] = receivedTask
+                        taskAdapter.notifyItemChanged(position)
+                        mainController.modifyTask(receivedTask)
+                    }
+                }
+            }
+        }
+
+        amb.taskRv.adapter = taskAdapter
+        amb.taskRv.layoutManager = LinearLayoutManager(this)
+
+        fillTaskList()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.add_contact_mi -> {
+                carl.launch(Intent(this, TaskActivity::class.java))
+                true
+            }
+            else -> { false }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onTaskClick(position: Int) {
+        Intent(this, TaskActivity::class.java).apply {
+            putExtra(EXTRA_TASK, taskList[position])
+            putExtra(EXTRA_VIEW_TASK, true)
+            startActivity(this)
+        }
+    }
+
+    override fun onRemoveTaskMenuItemClick(position: Int) {
+        mainController.removeTask(taskList[position])
+        taskList.removeAt(position)
+        taskAdapter.notifyItemRemoved(position)
+        Toast.makeText(this, "Task removed!", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onEditTaskMenuItemClick(position: Int) {
+        Intent(this, TaskActivity::class.java).apply {
+            putExtra(EXTRA_TASK, taskList[position])
+            carl.launch(this)
+        }
+    }
+
+    private fun fillTaskList(){
+        taskList.clear()
+
+        MainScope().launch {
+            val tasks = withContext(Dispatchers.IO) {
+                mainController.getTasks()
+            }
+
+            taskList.addAll(tasks)
+            taskAdapter.notifyDataSetChanged()
         }
     }
 }
