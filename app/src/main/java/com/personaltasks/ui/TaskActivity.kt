f0a4acde8b80a30138a6df99563683f494967896
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-05-15 16:58:29 -0300

    feat: add DatePickerDialog

--- app/src/main/java/com/personaltasks/ui/TaskActivity.kt
+++ app/src/main/java/com/personaltasks/ui/TaskActivity.kt
@@ -1,5 +1,6 @@
 package com.personaltasks.ui
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
@@ -9,8 +10,12 @@ import com.personaltasks.databinding.ActivityTaskBinding
 import com.personaltasks.model.Constant.EXTRA_TASK
 import com.personaltasks.model.Constant.EXTRA_VIEW_TASK
 import com.personaltasks.model.Task
+import java.time.LocalDate
+import java.util.Calendar
 
 class TaskActivity : AppCompatActivity() {
+    private var selectedDate: LocalDate? = null
+
     private val atb: ActivityTaskBinding by lazy {
         ActivityTaskBinding.inflate(layoutInflater)
     }
@@ -21,6 +26,27 @@ class TaskActivity : AppCompatActivity() {
 
         setSupportActionBar(atb.toolbarIn.toolbar)
         supportActionBar?.subtitle = "New contact"
+
+        val calendar = Calendar.getInstance()
+
+        atb.openDialogBt.setOnClickListener {
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(this,
+                { _, selectedYear, selectedMonth, selectedDayOfMonth ->
+                    val monthCorrected = selectedMonth + 1
+                    selectedDate = LocalDate.of(selectedYear, monthCorrected, selectedDayOfMonth)
+
+                    atb.openDialogBt.text = selectedDate.toString()
+                },
+                year,
+                month,
+                day
+            )
+            datePickerDialog.show()
+        }
 
         val receivedTask = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             intent.getParcelableExtra(EXTRA_TASK, Task::class.java)
@@ -33,12 +59,17 @@ class TaskActivity : AppCompatActivity() {
             with(atb) {
                 titleEt.setText(it.title)
                 descriptionEt.setText(it.description)
+                atb.openDialogBt.visibility = View.VISIBLE
+                atb.dateTv.visibility = View.GONE
 
                 val viewContact = intent.getBooleanExtra(EXTRA_VIEW_TASK, false)
                 if (viewContact) {
                     supportActionBar?.subtitle = "View task"
                     titleEt.isEnabled = false
                     descriptionEt.isEnabled = false
+                    atb.openDialogBt.visibility = View.GONE
+                    atb.dateTv.visibility = View.VISIBLE
+                    atb.dateTv.text = it.dueDate
                     saveBt.visibility = View.GONE
                 }
             }
@@ -49,7 +80,9 @@ class TaskActivity : AppCompatActivity() {
                 Task(
                     receivedTask?.id?:hashCode(),
                     titleEt.text.toString(),
-                    descriptionEt.text.toString()
+                    descriptionEt.text.toString(),
+                    selectedDate.toString()
+
                 ).let { contact ->
                     Intent().apply {
                         putExtra(EXTRA_TASK, contact)
