d1d319be974f19cbcdaa22b9811076d93b325599
Luiz Henrique do Nascimento Silva <luizhnsc@gmail.com>
2025-06-21 19:05:57 -0300

    feat: add EXTRA_TASK_ARRAY constant to Constant object

--- app/src/main/java/com/personaltasks/model/Constant.kt
+++ app/src/main/java/com/personaltasks/model/Constant.kt
@@ -4,4 +4,5 @@ object Constant {
     const val EXTRA_TASK = "EXTRA_TASK"
     const val EXTRA_VIEW_TASK = "EXTRA_VIEW_TASK"
     const val INVALID_TASK_ID = -1
+    const val EXTRA_TASK_ARRAY = "EXTRA_TASK_ARRAY"
 }
